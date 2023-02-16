1ee379054303679b60c6a0dd28649b25848fcb8b
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-02-16 10:26:10 +0700

    handle api error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./src/routes/contact.route");
+const ApiError = require("./src/helpers/api-error");
 
 const app = express();
 
@@ -9,5 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/contacts", contactRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  const code = err.statusCode || 500;
+  const msg = err.message || "Internal Server Error";
+  return res.status(code).json({ message: msg });
+});
 
 module.exports = app;
